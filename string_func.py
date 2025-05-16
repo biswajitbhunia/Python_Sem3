2aa54d0412e7b05ca3085ec5ae001e4e749a06aa
biswajitbhunia <44657622+biswajitbhunia@users.noreply.github.com>
2025-05-16 16:42:56 +0530

    s

---
+++ string_func.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 16 16:39:16 2025
+
+@author: biswajit
+"""
+
+s = input('Enter your name')
+s1 = s.upper()  
+s2 = s.capitalize()
+s3 = s.title()
+print('Your name is:',s1,s2,s3)
+
