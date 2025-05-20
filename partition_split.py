4306ede85165f344034a4f322ad142d06355dc08
biswajitbhunia <44657622+biswajitbhunia@users.noreply.github.com>
2025-05-20 08:42:39 +0530

    Update partition_split.py

--- partition_split.py
+++ partition_split.py
@@ -10,3 +10,19 @@ print(s.partition(","))
 #Convert into list
 print('The given string into lIST')
 print(s.split())
+s="hello, world!, how, are you?"
+
+
+#Partition by the given partition character
+print(s.partition(","))
+
+
+#Convert into list
+print('The given string into lIST')
+print(s.split(','))
+
+print('hello32'.isalnum())
+print('he43llo'.isalnum())
+print('34hello'.isalnum())
+
+print('hello32'.isdigit())
